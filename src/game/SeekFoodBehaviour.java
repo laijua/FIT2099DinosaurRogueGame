0fd4a3641191819e89143d1aa010a4d5446b71d8
Michael-Laifu Chhua <mchh0003@student.monash.edu>
2020-10-16 08:54:13 +1100

    changes made to seek food behaviour

--- src/game/SeekFoodBehaviour.java
+++ src/game/SeekFoodBehaviour.java
@@ -2,7 +2,7 @@ package game;
 
 import edu.monash.fit2099.engine.*;
 
-public class SeekFoodBehaviour implements Behaviour {
+public class SeekFoodBehaviour extends CommonStuffBehaviour {
 
     @Override
     public Action getAction(Actor actor, GameMap map) {
@@ -11,44 +11,92 @@ public class SeekFoodBehaviour implements Behaviour {
         Location dinosaurLocation = map.locationOf(actor);
         int dinoX = dinosaurLocation.x();
         int dinoY = dinosaurLocation.y();
-        boolean foundFood = false;
 
-        int[] dinosaurSearchRadius = {0, 1, -1, 2, -2, 3, -3, 4, -4};
+        ;
+
+        if (dinosaur.getEdibleType() == GameCapability.HERBIVOREEDIBLE) {
+            if (dinosaurLocation.getGround() instanceof Grass) {
+
+                dinosaurLocation.setGround(new Dirt());
+                dinosaur.increaseFoodLevel(5);
+                System.out.println(actor + " eats Grass");
+            }
+
+        }
 
         for (int x : dinosaurSearchRadius) {
             for (int y : dinosaurSearchRadius) {
-                if (dinoX+x <=79 && dinoY+y<=25 && dinoX+x >=0 && dinoY+y>=0) {
+                if (dinoX + x <= 79 && dinoY + y <= 24 && dinoX + x >= 0 && dinoY + y >= 0) {
                     Location location = map.at(dinoX + x, dinoY + y);
                     for (Item items : location.getItems()) {
                         if (items instanceof Food) {
                             Food food = (Food) items;
                             if (food.hasCapability(dinosaur.getEdibleType())) {
-                                if (false) {
-                                    // increase dino food level, remove food in the returned EatAction?
-                                    foundFood = true;
+                                boolean canEat = true;
+                                if (food instanceof Egg) {
+                                    Egg egg = (Egg) food;
+                                    if (egg.getDinosaurToHatch() == dinosaur.getClass()) {
+                                        canEat = false;
+                                    }
                                 }
-                                return new MoveActorAction(location, "Ssdsdsdsdsdsd");
+                                if (canEat) {
+                                    if (distance(dinosaurLocation, location) == 1) {
+
+                                        dinosaur.increaseFoodLevel(food.getFoodLevelPoint());
+                                        location.removeItem(food);
+                                        System.out.println(actor + " eats " + food);
+                                        return null;
+
+                                    }
+                                    Location there = location;
+                                    int currentDistance = distance(dinosaurLocation, there);
+                                    for (Exit exit : dinosaurLocation.getExits()) {
+                                        Location destination = exit.getDestination();
+                                        if (destination.canActorEnter(actor)) {
+                                            int newDistance = distance(destination, there);
+                                            if (newDistance < currentDistance) {
+                                                if (!destination.containsAnActor()) {
+                                                    return new MoveActorAction(destination, exit.getName());
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+//                            if (!location.containsAnActor()) {
+//                                return new MoveActorAction(location, "");
+//                            }
+                            }
+                        }
+                    }
+                    if (location.getGround() instanceof Grass) {
+                        int currentDistance = distance(dinosaurLocation, location);
+                        for (Exit exit : dinosaurLocation.getExits()) {
+                            Location destination = exit.getDestination();
+                            if (destination.canActorEnter(actor)) {
+                                int newDistance = distance(destination, location);
+                                if (newDistance < currentDistance) {
+                                    if (!destination.containsAnActor()) {
+                                        return new MoveActorAction(destination, exit.getName());
+                                    }
+                                }
                             }
                         }
                     }
                 }
+
             }
         }
 
-        if (!foundFood) {
-            if (dinosaur.getEdibleType() == GameCapability.CARNIVOREEDIBLE) {
-                for (int x : dinosaurSearchRadius) {
-                    for (int y : dinosaurSearchRadius) {
-                        if (dinoX + x <= 79 && dinoY + y <= 24 && dinoX + x >= 0
-                            && dinoY + y >= 0) {
-                            Location location = map.at(dinoX + x, dinoY + y);
-                            if (location.containsAnActor()) {
-                                if (location.getActor() instanceof Dinosaur) {
-                                    Dinosaur target = (Dinosaur) location.getActor();
-                                    if (target.hasCapability(GameCapability.ALLOSAURATTACKABLE)) {
-                                        System.out.println("SD");
-                                        return new FollowBehaviour(target).getAction(target, map);
-                                    }
+        if (dinosaur.getEdibleType() == GameCapability.CARNIVOREEDIBLE) {
+            for (int x : dinosaurSearchRadius) {
+                for (int y : dinosaurSearchRadius) {
+                    if (dinoX + x <= 79 && dinoY + y <= 24 && dinoX + x >= 0 && dinoY + y >= 0) {
+                        Location location = map.at(dinoX + x, dinoY + y);
+                        if (location.containsAnActor()) {
+                            if (location.getActor() instanceof Dinosaur) {
+                                Dinosaur target = (Dinosaur) location.getActor();
+                                if (target.hasCapability(GameCapability.ALLOSAURATTACKABLE)) {
+                                    return new FollowBehaviour(target).getAction(actor, map);
                                 }
                             }
                         }
@@ -61,4 +109,16 @@ public class SeekFoodBehaviour implements Behaviour {
         return null;
     }
 
+//    /**
+//     * Compute the Manhattan distance between two locations.
+//     *
+//     * @param a the first location
+//     * @param b the first location
+//     * @return the number of steps between a and b if you only move in the four cardinal directions.
+//     */
+//    private int distance(Location a, Location b) {
+//        return Math.abs(a.x() - b.x()) + Math.abs(a.y() - b.y());
+//    }
 }
+
+
