b0c23dd90480321236de50c84eed5c27144f02e4
Michael-Laifu Chhua <mchh0003@student.monash.edu>
2020-10-16 08:33:51 +1100

    changes made to dinosaur corpse

--- src/game/DinosaurCorpse.java
+++ src/game/DinosaurCorpse.java
@@ -2,6 +2,7 @@ package game;
 
 public class DinosaurCorpse extends Food {
     public DinosaurCorpse() {
-        super(30, "corpse", 'c', true);
+        super(30, "corpse", 'C', true);
+        addCapability(GameCapability.CARNIVOREEDIBLE);
     }
 }
