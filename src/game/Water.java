6e96db650447c2e26182ff567025a5c4025e0e69
Michael-Laifu Chhua <mchh0003@student.monash.edu>
2020-10-27 12:14:43 +1100

    added canActorEnter to water

--- src/game/Water.java
+++ src/game/Water.java
@@ -1,5 +1,6 @@
 package game;
 
+import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Ground;
 
 public class Water extends Ground {
@@ -11,5 +12,16 @@ public class Water extends Ground {
    */
   public Water() {
     super('~');
+  }
+
+  @Override
+  /**
+   * Override this to implement impassable terrain, or terrain that is only passable if conditions are met.
+   *
+   * @param actor the Actor to check
+   * @return true
+   */
+  public boolean canActorEnter(Actor actor) {
+    return actor.hasCapability(GameCapability.FLY);
   }
 }
