110a7c658426f9a8f0fc5c40f7785d70fc658af9
Michael-Laifu Chhua <mchh0003@student.monash.edu>
2020-10-28 19:01:24 +1100

    stuff

--- src/game/SeekFoodBehaviour.java
+++ src/game/SeekFoodBehaviour.java
@@ -28,7 +28,6 @@ public class SeekFoodBehaviour extends CommonStuffBehaviour {
             int dinoY = dinosaurLocation.y();
 
             // grazes on grass that it is on top of
-//            if (dinosaur.getEdibleType() == GameCapability.HERBIVOREEDIBLE) {
             if (dinosaur.containsEdible(GameCapability.HERBIVOREEDIBLE)) {
                 if (dinosaurLocation.getGround() instanceof Grass) {
                     dinosaurLocation.setGround(new Dirt());
